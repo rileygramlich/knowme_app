112253cd1bd05f9b9f320ff0ac4c505a7af0fcfb
Neha Bhole <nehasbhole@gmail.com>
2023-01-03 16:36:39 -0500

    add paths, functions for question model

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     path('quizzes/create', views.QuizCreate.as_view(), name='quiz_create'),
     path('quizzes/<int:pk>/update', views.QuizUpdate.as_view(), name='quiz_update'),
     path('quizzes/<int:pk>/delete/', views.QuizDelete.as_view(), name='quiz_delete'),
-    path('quizzes/<int:quiz_id>/take', views.quiz_take_quiz, name='quiz_take_quiz')
+    path('quizzes/<int:quiz_id>/take', views.quiz_take_quiz, name='quiz_take_quiz'),
     #Question
+    path('questions/create/', views.QuestionsCreate.as_view(), name='questions_create'),
+    path('questions/<int:pk>/update/', views.QuestionsUpdate.as_view(), name='questions_update'),
+    path('questions/<int:pk>/delete/', views.QuestionsDelete.as_view(), name='questions_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -26,7 +27,7 @@ def signup(request):
     'error_message': error_message
   })
   
- # GENERAL PATHS 
+# GENERAL PATHS 
 def home(request):
   return render(request, 'home.html')
 
@@ -63,3 +64,12 @@ def quiz_take_quiz(request):
     })
 
 # QUESTION PATHS
+
+
+# QUESTION PATHS
+class QuestionCreate(LoginRequiredMixin, CreateView):
+  model = Question
+  fields = '__all__'
+
+class QuestionUpdate(LoginRequiredMixin, UpdateView):
+  model = Question

00ff8bef910c662c044fd6d9367e97d2233045e6
Anne <Anne@MacBook-Air.local>
2023-01-03 12:12:24 -0700

    committing

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
     #Quiz
-    path('quiz', views.quiz_form_index, name='quiz_form_index'),
+    path('quiz/', views.quiz_form_index, name='quiz_form_index'),
     path('quizzes/<int:quiz_id>', views.quizzes_detail, name='detail'),
     #Question
     path('accounts/signup/', views.signup, name='signup'),

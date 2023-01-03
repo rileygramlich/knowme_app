3484e3f05f855356ba368c665d7ccc2c7520ee6c
Riley G <gramlichriley@gmail.com>
2023-01-03 10:37:35 -0700

    edit views and urls with new names

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,14 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
     #Quiz
-    path('quizzes/', views.quizzes_index, name='index'),
-    path('quizzes/<int:quiz_id', views.quizzes_detail, name='detail'),
+    path('/quiz', views.quiz_form_index, name='quiz_form_index'),
+    path('quizzes/<int:quiz_id>', views.quizzes_detail, name='detail'),
     #Question
-    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
     path('', views.home, name='home'),
-    path('/test', views.test_form_index, name='test_form_index')
 
 ]

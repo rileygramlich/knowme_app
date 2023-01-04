4ebde588f0f444b1e3a2d0afed04b3d017b4a68f
Anne <Anne@MacBook-Air.local>
2023-01-04 12:51:44 -0700

    Adding edit and delete fxn to quiz and question

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     #Quiz
     path('quizzes/', views.quizzes_index, name='index'),
-    path('quizzes/<int:quiz_id>', views.quizzes_detail, name='detail'),
+    path('quizzes/<int:quiz_id>/', views.quizzes_detail, name='detail'),
     path('quizzes/create', views.QuizCreate.as_view(), name='quiz_create'),
     path('quizzes/<int:pk>/update', views.QuizUpdate.as_view(), name='quiz_update'),
     path('quizzes/<int:pk>/delete/', views.QuizDelete.as_view(), name='quiz_delete'),

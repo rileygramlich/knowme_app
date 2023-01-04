e871c0ea10f2e2285d1abd89a3159ff5ef61e8e0
Riley G <gramlichriley@gmail.com>
2023-01-04 09:31:36 -0700

    fix sign up redirect

--- main_app/views.py
+++ main_app/views.py
@@ -17,7 +17,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('quizzes/index')
+      return redirect('/quizzes')
     else:
       error_message = 'Invalid credentials - try again'
   form = UserCreationForm()

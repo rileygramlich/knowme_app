e776ace94a070a7eda267d71c30cdd1efab6fde7
Anne <Anne@MacBook-Air.local>
2023-01-05 15:21:19 -0700

    Score count is functioning

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,43 +1,42 @@
 console.log("Active");
 
 let btn = document.getElementById('btn');
-let radioButtons = document.querySelectorAll('input[name="answer"]');
 
 let true_answers = document.querySelectorAll(".true_answer");
 let false_answer1s = document.querySelectorAll(".false_answer1");
 let false_answer2s = document.querySelectorAll(".false_answer2");
 let false_answer3s = document.querySelectorAll(".false_answer3");
 
-btn.addEventListener('click', () => {
-    let selectedAnswer;
-    let score = 0
-    for (let i=0; i< true.answers.length; i++) {
-
-    }
-    for (let radioButton of radioButtons) {
-        if (radioButton.checked) {
-            selectedAnswer = radioButton.value;
-            if (selectedAnswer = true_answers) {
-                score++
-            }
-            break;
-        }
-    }  
-});
-
-
-
 for (let i=0; i<true_answers.length; i++) {
     let answersEl = document.querySelectorAll("ul")
-    let currentAns = [true_answers[i].value, false_answer1s[i].value, false_answer2s[i].value, false_answer3s[i].value]  
+    let currentAns = [[true_answers[i].value, 't'], [false_answer1s[i].value, 'f'], [false_answer2s[i].value, 'f'], [false_answer3s[i].value, 'f']]  
     currentAns = randomize(currentAns)
     currentAns.forEach(option => {
         let choice = document.createElement("li")
-        choice.innerHTML = "<input type= 'radio' name= 'text' value= '"+ option + "'><span>"+ option +"</span>"
+        choice.innerHTML = `<input type= 'radio' name= 'text' class= ${option[1]} value= ${option[0]}><span>${option[0]}</span>`
         answersEl[i].appendChild(choice)
     })
 };
+let radioButtons = document.querySelectorAll('input[name="text"]');
+console.log(radioButtons)
 
 function randomize(answers) {
     return answers.sort(() => Math.random() - 0.5)
 };
+
+let userAnswers = Array.from({ length: radioButtons.length / 4 })
+Array.from(radioButtons).forEach(radioButton => {
+    radioButton.addEventListener('change', (e) => {
+        let question = e.target.parentNode.parentNode.parentNode.id
+        userAnswers[question] = e.target.classList.contains('t')
+    })
+});
+
+let score = 0
+btn.addEventListener('click', () => {
+    score = userAnswers.filter(answer => answer == true).length
+    let resultEl = document.getElementById("results")
+    let result = document.createElement("span")
+    result.innerHTML = `You got ${score} out of ${userAnswers.length} correct!`
+    resultEl.appendChild(result)
+});

fb7f565c7fa783528a60c046629105092eb492fb
Neha Bhole <nehasbhole@gmail.com>
2023-01-05 17:15:27 -0500

    add heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn knowme_app.wsgi
--- knowme_app/settings.py
+++ knowme_app/settings.py
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/quizzes/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Configure Django App for Heroku
+import django_heroku
+django_heroku.settings(locals())
